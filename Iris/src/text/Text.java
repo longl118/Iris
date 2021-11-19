4f8a05ac802e5bbc8e9cc87070e7d618e7373391
longl118 <1101576544@qq.com>
2021-11-19 15:06:49 +0800

    first Iris

---
+++ Iris/src/text/Text.java
@@ -0,0 +1,52 @@
+package text;
+
+import java.io.IOException;
+
+public class Text {
+    public static void main(String args[]) throws IOException {
+        //存放数据以及保存模型文件路径
+        String filepath = "D:\\ProgramData\\wenjian\\iris\\";
+        /*
+         * -s 设置svm类型：默认值为0
+         *          0– C-SVC
+         *          1 – v-SVC
+         *          2 – one-class-SVM
+         *          3 –ε-SVR
+         *          4 – n - SVR
+         *
+         * -t 设置核函数类型，默认值为2
+         *          0 --线性核
+         *          1 --多项式核
+         *          2 -- RBF核
+         *          3 -- sigmoid核
+         *
+         * -d degree:设置多项式核中degree的值，默认为3
+         *
+         * -c cost：设置C-SVC、ε-SVR、n - SVR中从惩罚系数C，默认值为1；
+         */
+        String[] arg = {"-s","0","-c","10","-t","0",filepath+"iris1.txt",filepath+"line.txt"};
+        String[] arg1 = {filepath+"iris2.txt",filepath+"line.txt",filepath+"predict1.txt"};
+        System.out.println("----------------线性-----------------");
+        //训练函数
+        svm_train.main(arg);
+        svm_predict.main(arg1);
+
+
+        arg[5]="1";
+        arg[7]=filepath+"poly.txt";//输出文件路径
+        arg1[1]=filepath+"poly.txt";
+        arg1[2]=filepath+"predict2.txt";
+        System.out.println("---------------多项式-----------------");
+        svm_train.main(arg);
+        svm_predict.main(arg1);
+
+        arg[5]="2";
+        arg[7]=filepath+"RBF.txt";
+        arg1[1]=filepath+"RBF.txt";
+        arg1[2]=filepath+"predict3.txt";
+        System.out.println("---------------高斯核-----------------");
+        svm_train.main(arg);
+        svm_predict.main(arg1);
+
+    }
+}
